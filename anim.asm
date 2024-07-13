05b933a6c14de9f9824ce2a142d36f415f17a3eb
John Eblen <jeblen@acm.org>
2024-07-13 15:55:28 -0400

    Code to test moving shapes horizontally with shape shifting
    This improves ce83d4e by making the animation smooth with a new
    subroutine for shape shifting.

--- anim.asm
+++ anim.asm
@@ -1,13 +1,12 @@
 ; Code to test moving of shapes horizontally. The result should be a tiny image
 ; of a man and woman, roughly in the middle of the screen, that moves from far
-; left to far right. Movement is not yet smooth (no shape shifting) but drawing
-; routine now uses pixel (instead of byte) coordinates for horizontal position
-; and width.
+; left to far right. Movement should now be smooth with the new shape shifting
+; subroutine.
 
 ; Shape table (man and woman)
-org 5FF2
+org 5FEB
 .shape_table
-data 0602081C3E2A5D081C5D14142222
+data 000603081C003E2A005D08001C5D00141400222200
 
 org 6000
 
@@ -81,7 +80,7 @@ bne   .div7_table_fill_loop
 
 ; -------------------- Set shape and draw constants --------------------
 label shape_addr_h 5F
-label shape_addr_l F2
+label shape_addr_l EB
 label xcoord 50
 label ycoord 00
 label delay  FF
@@ -103,7 +102,7 @@ staz  .shape_coords 1
 ; Compute number of iterations to move shape to end of screen
 
 ; Compute negative of width in number of pixels
-ldaa  .shape_table 1
+ldaa  .shape_table 2
 asl
 asl
 asl
@@ -122,6 +121,9 @@ sbci  .ycoord
 jsra  .draw_shape
 pha
 ldai  .delay
+jsra  .wait
+jsra  .wait
+jsra  .wait
 jsra  .wait
 pla
 jsra  .draw_shape
@@ -160,19 +162,25 @@ pha
 ldaz  .shape_coords
 pha
 
+; Shift shape
+; Do first since subroutine does not preserve registers
+ldxz  .shape_coords 1
+jsra  .shift_shape_table
+
 ; Set shape table address in load instruction.
 ; Warning: We only increment address low byte by two to get to raw shape data.
-; This will not work across a page boundary nor when we add more header data.
+; This will not work if the low byte overflows or if we add more header data.
 ; TODO: Create a more robust solution for this problem.
 ldxz  .shape_table_addr
 inx
 inx
-stxa  .shape_table_load_instr 1
+inx
+stxa  .draw_shape_table_load_instr 1
 ldaz  .shape_table_addr 1
-staa  .shape_table_load_instr 2
+staa  .draw_shape_table_load_instr 2
 
 ; Set X and maximum X (in .shape_coords)
-ldai  00
+ldai  01
 tay
 ldxz  .shape_coords
 txa
@@ -188,7 +196,7 @@ zbyte y_start
 staz  .y_start
 
 ; Set maximum Y (in .shape_coords + 1 in bytes)
-ldai  01
+ldai  02
 tay
 ldaz  .y_start
 clc
@@ -203,17 +211,17 @@ ldyz  .y_start
 jsra  .line_index_to_address
 
 ; Write to screen
-.shape_table_load_instr
+.draw_shape_table_load_instr
 ldaa  0000
 eorny .line_address
 stany .line_address
 
 ; Increment above load address for next loop
-inca  .shape_table_load_instr 1
-ldaa  .shape_table_load_instr 1
-bne   .no_shape_table_addr_overflow
-inca  .shape_table_load_instr 2
-.no_shape_table_addr_overflow
+inca  .draw_shape_table_load_instr 1
+ldaa  .draw_shape_table_load_instr 1
+bne   .no_shape_table_addr_overflow_while_drawing
+inca  .draw_shape_table_load_instr 2
+.no_shape_table_addr_overflow_while_drawing
 
 ; Check for end of row
 iny
@@ -239,6 +247,198 @@ pla
 rts
 ; -------------------- End subroutine --------------------
 
+; Subroutine to shift a shape table for a given column
+; Input:  Zero-page shape table address (not table itself) in "shape_table_addr"
+;         Column in X
+; Output: Shape table is shifted for drawing at the given column 
+; Note:   Does not preserve registers. shape_table_addr is not changed.
+.shift_shape_table
+
+; Store shape table size now while A and Y are free
+zbyte shape_table_size 2
+ldai  01
+tay
+ldany .shape_table_addr
+staz  .shape_table_size
+iny
+ldany .shape_table_addr
+staz  .shape_table_size 1
+
+; Set Y = 0 and A = X (column)
+ldai  00
+tay
+txa
+
+; Compute shift value from column C, which is C % 7
+; Formula is C % 7 = ( (C % 8) + (C / 7) ) % 8
+andi  07
+clc
+adcax .div7_table_addr
+andi  07
+
+; Compare old and new shift values and use different routines for shift left vs
+; shift right. Store the difference in "shift_counter" and don't forget to
+; store new shift value into shape table when done.
+
+; cache new shift value
+tax
+
+; Compute and store the difference
+sec
+sbcny .shape_table_addr
+zbyte shift_counter
+staz  .shift_counter
+
+; Store new shift value to shape table
+txa
+stany .shape_table_addr
+
+; Branch to correct routine
+; The two routines are very similar, differing mostly in the opcodes used
+; (adc vs sbc, rol vs ror, inc vs dec, bmi vs bpl, etc.)
+bcs   .shift_right_outer_loop
+
+; Routine for shifting left
+decz  .shift_counter
+
+.shift_left_outer_loop
+incz  .shift_counter
+bpl   .end_left_shifting
+
+; Initialize shape table address in two instructions
+; Warning: We only increment address low byte by three to get to raw shape data.
+; This will not work if the low byte overflows or if we add more header data.
+; TODO: Create a more robust solution for this problem.
+ldxz  .shape_table_addr
+inx
+inx
+inx
+stxa  .shift_shape_table_left_load_instr   1
+stxa  .shift_shape_table_left_store_instr  1
+ldxz  .shape_table_addr 1
+stxa  .shift_shape_table_left_load_instr   2
+stxa  .shift_shape_table_left_store_instr  2
+
+; Initialize X and Y.
+ldai  00
+tax
+ldyz  .shape_table_size 1
+dey
+
+.shift_left_middle_loop
+clc
+.shift_left_inner_loop
+
+; Do the shifting. Because only 7 bits are used for the shape, we manually
+; set the carry for the next rotation based on the new value of bit 7. Ignore
+; bit 7 otherwise, but this will probably change once we start handling colors.
+.shift_shape_table_left_load_instr
+ldaay 0000
+ror
+bpl   .no_carry_on_left_shift
+orai  40
+bne   .carry_on_left_shift
+.no_carry_on_left_shift
+andi  BF
+.carry_on_left_shift
+.shift_shape_table_left_store_instr
+staay 0000
+   
+; Check for end of row
+dey
+bpl   .shift_left_inner_loop
+
+; Increment above addresses for next inner loop
+ldaa  .shift_shape_table_left_load_instr 1
+clc
+adcz  .shape_table_size 1
+staa  .shift_shape_table_left_load_instr  1
+staa  .shift_shape_table_left_store_instr 1
+bvc   .no_shape_table_addr_overflow_while_shifting_left
+inca  .shift_shape_table_left_load_instr  2
+inca  .shift_shape_table_left_store_instr 2
+.no_shape_table_addr_overflow_while_shifting_left
+
+; Reset row and check if this is last row
+; Use eor to preserve the carry flag
+ldyz  .shape_table_size 1
+dey
+inx
+cpxz  .shape_table_size
+bne   .shift_left_middle_loop
+
+; Start over in case another shift is needed
+beq   .shift_left_outer_loop
+
+.end_left_shifting
+rts
+
+; Routine for shifting right
+.shift_right_outer_loop
+
+decz  .shift_counter
+bmi   .end_right_shifting
+
+; Initialize shape table address in two instructions
+; Warning: We only increment address low byte by three to get to raw shape data.
+; This will not work if the low byte overflows or if we add more header data.
+; TODO: Create a more robust solution for this problem.
+ldxz  .shape_table_addr
+inx
+inx
+inx
+stxa  .shift_shape_table_right_instr 1
+stxa  .shift_shape_table_right_instr 4
+ldxz  .shape_table_addr 1
+stxa  .shift_shape_table_right_instr 2
+stxa  .shift_shape_table_right_instr 5
+
+; Initialize X (Y = 0 already)
+ldai  00
+tax
+
+.shift_right_middle_loop
+clc
+.shift_right_inner_loop
+
+; Do the shifting. Because only 7 bits are used for the shape, we manually
+; set the carry for the next rotation based on the new value of bit 7. Ignore
+; bit 7 otherwise, but this will probably change once we start handling colors.
+.shift_shape_table_right_instr
+rola  0000
+bita  0000
+clc
+bpl   .no_carry_on_right_shift
+sec
+.no_carry_on_right_shift
+   
+; Increment above addresses for next loop
+inca  .shift_shape_table_right_instr 1
+inca  .shift_shape_table_right_instr 4
+bne   .no_shape_table_addr_overflow_while_shifting_right
+inca  .shift_shape_table_right_instr 2
+inca  .shift_shape_table_right_instr 5
+.no_shape_table_addr_overflow_while_shifting_right
+
+; Check for end of row
+; Use eor to preserve the carry flag
+iny
+tya
+eorz  .shape_table_size 1
+bne   .shift_right_inner_loop
+
+; Reset row and check if this is last row
+ldai  00
+tay
+inx
+cpxz  .shape_table_size
+bne   .shift_right_middle_loop
+
+; Start over in case another shift is needed
+beq   .shift_right_outer_loop
+
+.end_right_shifting
+rts
 
 ; Subroutine to convert line index (0-191) into line address
 ; Input:  line index in X
